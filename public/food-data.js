37e837344c989a0519ad30b395459ea838833a1f
shafik720 <shafikrasel5@gmail.com>
2022-11-30 00:02:58 +0600

    working on data

---
+++ public/food-data.js
@@ -0,0 +1,51 @@
+
+let foodData = [
+    {
+        id : 1,
+        foodName : 'Bagel and Creame Cheese',
+        shortDesc : 'Italian BreakFast',
+        price : 5.85,
+        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        catagory : 'breakfast'
+    },
+    {
+        id : 1,
+        foodName : 'Bagel and Creame Cheese',
+        shortDesc : 'Italian BreakFast',
+        price : 5.85,
+        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        catagory : 'breakfast'
+    },
+    {
+        id : 1,
+        foodName : 'Bagel and Creame Cheese',
+        shortDesc : 'Italian BreakFast',
+        price : 5.85,
+        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        catagory : 'breakfast'
+    },
+    {
+        id : 1,
+        foodName : 'Bagel and Creame Cheese',
+        shortDesc : 'Italian BreakFast',
+        price : 5.85,
+        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        catagory : 'breakfast'
+    },
+    {
+        id : 1,
+        foodName : 'Bagel and Creame Cheese',
+        shortDesc : 'Italian BreakFast',
+        price : 5.85,
+        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        catagory : 'breakfast'
+    },
+    {
+        id : 1,
+        foodName : 'Bagel and Creame Cheese',
+        shortDesc : 'Italian BreakFast',
+        price : 5.85,
+        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        catagory : 'breakfast'
+    }
+]
