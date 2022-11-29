69a0c4b8fa00f67a7f803b87a9152fe52d27ea98
shafik720 <shafikrasel5@gmail.com>
2022-11-29 21:02:18 +0600

    successfully implemented outlet

--- src/Pages/Home/HomeHeader/HomeHeader.js
+++ src/Pages/Home/HomeHeader/HomeHeader.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './HomeHeader.css'
 
 const HomeHeader = () => {
     return (
-        <div className='mb-5'>
-            <Link to='/'>Breakfast</Link>
-            <Link to='/lunch'>Lunch</Link>
-            <Link to='/dinner'>Dinner</Link>
+        <div className='homeHeaderMenu mb-5'>
+            <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Breakfast</NavLink>
+            <NavLink to='/lunch' className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Lunch</NavLink>
+            <NavLink to='/dinner' className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Dinner</NavLink>
         </div>
     );
 };
