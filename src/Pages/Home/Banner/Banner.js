7788dc20b641b937b3d7528170963e45573e6c67
shafik720 <shafikrasel5@gmail.com>
2022-11-29 19:53:44 +0600

    working on banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,12 +3,12 @@ import './Banner.css';
 
 const Banner = () => {
     return (
-        <div className='banner-div text-center'>
+        <div className='banner-div container-fluid text-center'>
             <div className="banner-header">
                 <h1>Heart is Where the Food is</h1>
             </div>
             <div className="banner-search">
-                <input type="text" name="" id="" />
+                <input type="text" name="" id="" placeholder='Search Your Food Here . . .'/>
                 <button>Search</button>
             </div>
         </div>
