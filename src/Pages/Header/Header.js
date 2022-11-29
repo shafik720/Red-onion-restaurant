3361b358ba1ffe556a763b7f4f33cbe6a015f12a
shafik720 <shafikrasel5@gmail.com>
2022-11-29 21:38:01 +0600

    making navbar responsive

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -7,19 +7,24 @@ import { faCartShopping, faBars } from '@fortawesome/free-solid-svg-icons';
 
 
 const Header = () => {
+    function menuShow(){
+        const rightHeader = document.querySelector('.right-header');
+
+        rightHeader.classList.add('active');
+    }
     return (
         <div>
-            <div className="container">
+            <div className="container header-div-parent">
                 <div className="header-div">
                     <div className="left-header">
                         <Link to='/'><img src={headerLogo} alt="" /></Link>
                     </div>
-                    <div className="right-header">
+                    <div className="right-header ">
                         <Link to='/cart'><FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon></Link>
                         <Link className='commonLink' to='/login'>Login</Link>
                         <Link className='commonLink' to='/signup'>Sign Up</Link>
                     </div>                    
-                    <div className="mobile-menu">
+                    <div onClick={menuShow} className="mobile-menu">
                             <span><FontAwesomeIcon icon={faBars}></FontAwesomeIcon></span>
                         </div>
                 </div>
