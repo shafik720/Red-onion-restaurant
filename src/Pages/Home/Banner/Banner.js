bf4cd8b82273a10012f294027d19ca3fb4867bc0
shafik720 <shafikrasel5@gmail.com>
2022-11-29 19:37:14 +0600

    working on banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div>
+            <h2>Banner Section Here</h2>
+        </div>
+    );
+};
+
+export default Banner;
