1ac5f577cee0db0e052f3d721081f9bc89a28e68
shafik720 <shafikrasel5@gmail.com>
2022-11-29 19:14:23 +0600

    working on header

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import headerLogo from '../../utilities/images/logo2.png';
 
 const Header = () => {
     return (
         <div>
-            <h2>Header Section</h2>
+            <div className="container">
+                <div className="header-div">
+                    <div className="left-header">
+                        <img src={headerLogo} alt="" />
+                    </div>
+                    <div className="right-header"></div>
+                </div>
+            </div>
         </div>
     );
 };
