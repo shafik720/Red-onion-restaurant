f6fef6f071ae2c1ad5f9d6b66a5d4c4056469650
shafik720 <shafikrasel5@gmail.com>
2022-11-30 00:09:54 +0600

    working on data

--- public/food-data.js
+++ public/food-data.js
@@ -9,43 +9,43 @@ let foodData = [
         catagory : 'breakfast'
     },
     {
-        id : 1,
-        foodName : 'Bagel and Creame Cheese',
-        shortDesc : 'Italian BreakFast',
-        price : 5.85,
-        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        id : 2,
+        foodName : 'Breakfast Sandwitch',
+        shortDesc : 'American BreakFast',
+        price : 9.99,
+        img : 'https://i.ibb.co/sm3WKxW/breakfast2.png',
         catagory : 'breakfast'
     },
     {
-        id : 1,
-        foodName : 'Bagel and Creame Cheese',
-        shortDesc : 'Italian BreakFast',
-        price : 5.85,
-        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        id : 3,
+        foodName : 'Chicken Jinga lala',
+        shortDesc : 'Indian BreakFast',
+        price : 6.50,
+        img : 'https://i.ibb.co/8snX8JD/breakfast3.png',
         catagory : 'breakfast'
     },
     {
-        id : 1,
-        foodName : 'Bagel and Creame Cheese',
-        shortDesc : 'Italian BreakFast',
-        price : 5.85,
-        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        id : 4,
+        foodName : 'Double Dim',
+        shortDesc : 'Bangladeshi BreakFast',
+        price : 3.00,
+        img : 'https://i.ibb.co/kMq0G6x/breakfast4.png',
         catagory : 'breakfast'
     },
     {
-        id : 1,
-        foodName : 'Bagel and Creame Cheese',
-        shortDesc : 'Italian BreakFast',
-        price : 5.85,
-        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        id : 5,
+        foodName : 'Toast Coissant Fried Egg',
+        shortDesc : 'French BreakFast',
+        price : 5.50,
+        img : 'https://i.ibb.co/T86PNPT/breakfast5.png',
         catagory : 'breakfast'
     },
     {
-        id : 1,
-        foodName : 'Bagel and Creame Cheese',
-        shortDesc : 'Italian BreakFast',
-        price : 5.85,
-        img : 'https://i.ibb.co/2F4hNKt/breakfast1.png',
+        id : 6,
+        foodName : 'Fried Egg Toast Branch',
+        shortDesc : 'Persian BreakFast',
+        price : 7.00,
+        img : 'https://i.ibb.co/Xk4Jpty/breakfast6.png',
         catagory : 'breakfast'
     }
 ]
