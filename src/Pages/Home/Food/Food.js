cb23671ae8f56d454f7ad4a6401b8b27d089fa8f
shafik720 <shafikrasel5@gmail.com>
2022-11-30 13:01:21 +0600

    before moving to complex function

--- src/Pages/Home/Food/Food.js
+++ src/Pages/Home/Food/Food.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './Food.css'
 
 const Food = (props) => {
-    const{foodName, shortDesc, price, catagory, img} = props.index ; 
+    const{foodName, shortDesc, price, catagory, img, id} = props.index ; 
+    function foodCardClick(element){
+        console.log(element);
+    }
     return (
-        <div className='col-lg-4 col-md-6   g-4'>
+        <div onClick={()=>foodCardClick(id)} className='col-lg-4 col-md-6   g-4'>
             <div className="food-card h-100">
             <img src={img} alt="" />
             <h2>{foodName} </h2>
