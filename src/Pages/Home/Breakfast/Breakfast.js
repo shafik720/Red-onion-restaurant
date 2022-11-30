3cc35c0612a071fc8d5307f9c9ffd8eab33c75a4
shafik720 <shafikrasel5@gmail.com>
2022-11-30 11:34:07 +0600

    working on breakfast

--- src/Pages/Home/Breakfast/Breakfast.js
+++ src/Pages/Home/Breakfast/Breakfast.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Breakfast.css'
 
 const Breakfast = () => {
+    const[breakfast, setBreakfast] = useState([]);
+    useEffect(()=>{
+        fetch('https://raw.githubusercontent.com/shafik720/rest-api/main/red-onion.json')
+        .then(res=>res.json())
+        .then(data=>setBreakfast(data))
+    },[])
     return (
         <div>
             <h2>Break Fast Section Here </h2>
---
+++ src/Pages/Home/Food/Food.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Food.css'
+
+const Food = (props) => {
+    return (
+        <div>
+            <h2>Food Name : </h2>
+        </div>
+    );
+};
+
+export default Food;
