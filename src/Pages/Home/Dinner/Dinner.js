4faa30c6497e6cfd89f7644e0835f1817b60a1ab
shafik720 <shafikrasel5@gmail.com>
2022-11-30 12:37:42 +0600

    dinner done

--- src/Pages/Home/Dinner/Dinner.js
+++ src/Pages/Home/Dinner/Dinner.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Food from '../Food/Food';
 import './Dinner.css';
 
 const Dinner = () => {
+    const [dinner, setDinner] = useState([]);
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/shafik720/rest-api/main/red-onion.json')
+            .then(res => res.json())
+            .then(data => setDinner(data))
+    }, [])
     return (
-        <div>
-            <h2>Dinner Section here</h2>
+        <div className="my-5">
+            <div className="container ">
+            <div className=' row food-showcase-section'>
+                        {
+                            dinner.map(index => index.catagory == 'dinner' && <Food
+                                index={index}
+                                key={index.id}
+                            ></Food>)
+                        }
+                    </div>
+            </div>
         </div>
     );
 };
