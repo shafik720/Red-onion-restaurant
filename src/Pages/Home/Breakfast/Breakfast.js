a84074bbf7d21e763f09910bd3da1f64c49c86aa
shafik720 <shafikrasel5@gmail.com>
2022-11-30 11:52:30 +0600

    working on breakfast

--- src/Pages/Home/Breakfast/Breakfast.js
+++ src/Pages/Home/Breakfast/Breakfast.js
@@ -3,20 +3,24 @@ import Food from '../Food/Food';
 import './Breakfast.css'
 
 const Breakfast = () => {
-    const[breakfast, setBreakfast] = useState([]);
-    useEffect(()=>{
+    const [breakfast, setBreakfast] = useState([]);
+    useEffect(() => {
         fetch('https://raw.githubusercontent.com/shafik720/rest-api/main/red-onion.json')
-        .then(res=>res.json())
-        .then(data=>setBreakfast(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBreakfast(data))
+    }, [])
     return (
-        <div>
-            {
-                breakfast.map(index=><Food 
-                    index = {index}
-                    key = {index.id}
-                ></Food>)
-            }
+        <div className="my-5">
+            <div className="container ">
+            <div className=' row food-showcase-section'>
+                        {
+                            breakfast.map(index => <Food
+                                index={index}
+                                key={index.id}
+                            ></Food>)
+                        }
+                    </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Food/Food.js
+++ src/Pages/Home/Food/Food.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './Food.css'
 
 const Food = (props) => {
+    const{foodName, shortDesc, price, catagory, img} = props.index ; 
     return (
-        <div>
-            <h2>Food Name : </h2>
+        <div className='col-lg-4 col-md-6   g-4'>
+            <div className="food-card h-100">
+            <img src={img} alt="" />
+            <h2>{foodName} </h2>
+            <p> {shortDesc} </p>
+            <p>$ {price} </p>
+            </div>
         </div>
     );
 };
