daa6650f128b7b494b913fa5d9a27ee53bc6031d
shafik720 <shafikrasel5@gmail.com>
2022-11-30 13:43:14 +0600

    working on single food display

--- src/Pages/FoodDisplay/FoodDisplayTwo/FoodDisplayTwo.js
+++ src/Pages/FoodDisplay/FoodDisplayTwo/FoodDisplayTwo.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './FoodDisplayTwo.css';
 
 const FoodDisplayTwo = (props) => {
+    const{foodName, shortDesc, price, catagory, img, id} = props.index ; 
     return (
-        <div>
-            <h2>Food Name : {props.index.foodName} </h2>
+        <div className='container'>
+            <div className="row">
+                <div className="col-lg-6">
+                    <div className="">
+                        <h1>{foodName}</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa minima asperiores sequi odit eligendi veniam? Porro voluptatibus blanditiis facere. Quod, esse numquam. Temporibus tempore fugit modi illo ipsa eum dignissimos. Lorem ipsum, dolor sit </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
