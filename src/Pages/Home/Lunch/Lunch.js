422ee31d7524004a4ee289d03b32797ddb51c093
shafik720 <shafikrasel5@gmail.com>
2022-11-30 12:17:35 +0600

    working on breakfast

--- src/Pages/Home/Lunch/Lunch.js
+++ src/Pages/Home/Lunch/Lunch.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Food from '../Food/Food';
 import './Lunch.css';
 
 const Lunch = () => {
+    const [lunch, setLunch] = useState([]);
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/shafik720/rest-api/main/red-onion.json')
+            .then(res => res.json())
+            .then(data => setLunch(data))
+    }, [])
     return (
-        <div>
-            <h2>Lunch Section Here</h2>
+        <div className="my-5">
+            <div className="container ">
+            <div className=' row food-showcase-section'>
+                        {
+                            lunch.map(index => <Food
+                                index={index}
+                                key={index.id}
+                            ></Food>)
+                        }
+                    </div>
+            </div>
         </div>
     );
 };
