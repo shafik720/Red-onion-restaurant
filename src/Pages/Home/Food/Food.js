888daa4b6c7267085a90c7c17798a90d43dadc14
shafik720 <shafikrasel5@gmail.com>
2022-12-01 14:34:07 +0600

    working on single food display route

--- src/Pages/Home/Food/Food.js
+++ src/Pages/Home/Food/Food.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Food.css'
+import './Food.css';
 
 const Food = (props) => {
     const{foodName, shortDesc, price, catagory, img, id} = props.index ; 
