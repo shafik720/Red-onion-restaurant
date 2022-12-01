a62de01264e9168e30838f5def653d6cea3b94b2
shafik720 <shafikrasel5@gmail.com>
2022-12-01 11:26:28 +0600

    creating custom hook

--- src/Pages/Home/Breakfast/Breakfast.js
+++ src/Pages/Home/Breakfast/Breakfast.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import useFood from '../../../utilities/hooks/useFood';
 import Food from '../Food/Food';
 import './Breakfast.css'
 
 const Breakfast = () => {
-    const [breakfast, setBreakfast] = useState([]);
-    useEffect(() => {
-        fetch('https://raw.githubusercontent.com/shafik720/rest-api/main/red-onion.json')
-            .then(res => res.json())
-            .then(data => setBreakfast(data))
-    }, [])
+    const [breakfast, setBreakfast] = useFood();
     return (
         <div className="my-5">
             <div className="container ">
--- src/Pages/Home/Lunch/Lunch.js
+++ src/Pages/Home/Lunch/Lunch.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import useFood from '../../../utilities/hooks/useFood';
 import Food from '../Food/Food';
 import './Lunch.css';
 
 const Lunch = () => {
-    const [lunch, setLunch] = useState([]);
-    useEffect(() => {
-        fetch('https://raw.githubusercontent.com/shafik720/rest-api/main/red-onion.json')
-            .then(res => res.json())
-            .then(data => setLunch(data))
-    }, [])
+    const [lunch, setLunch] = useFood();
     return (
         <div className="my-5">
             <div className="container ">
