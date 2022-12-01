5a6168f5897255ddc49e26cabf026753b0ed18c3
shafik720 <shafikrasel5@gmail.com>
2022-12-01 14:39:19 +0600

    working on single food display route

--- src/Pages/FoodDisplay/FoodDisplayTwo/NextFoodDisplay/NextFoodDisplay.js
+++ src/Pages/FoodDisplay/FoodDisplayTwo/NextFoodDisplay/NextFoodDisplay.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import './NextFoodDisplay.css';
 
 const NextFoodDisplay = (props) => {
     const{ foodName, price, img, id} = props.index ; 
+
+    let navigate = useNavigate();
+    function foodCardClick(element){
+        let url = `/food/${element}`;
+        navigate(url);
+    }
     return (
         <div className='col-4 next-food-card '>
             <div className="text-center h-100  ">
-                <div className="h-100 next-food-single-item ">
+                <div  onClick={()=>foodCardClick(id)} className="h-100 next-food-single-item ">
                 <img src={img} alt="" />
                 <p className="fw-bold mt-3">{foodName}</p>
                 </div>
