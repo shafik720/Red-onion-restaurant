f75e3cbf0707957394c614a308d0286722065e17
shafik720 <shafikrasel5@gmail.com>
2022-12-01 14:42:59 +0600

    working on single food display route

--- src/Pages/FoodDisplay/FoodDisplayTwo/FoodDisplayTwo.js
+++ src/Pages/FoodDisplay/FoodDisplayTwo/FoodDisplayTwo.js
@@ -55,7 +55,7 @@ const FoodDisplayTwo = (props) => {
             <div className="row my-5">
                 <div className="col-1 ">
                     <div className="next-icon  h-100">
-                        <p className="m-0 fs-2"><FontAwesomeIcon icon={faHandPointLeft}></FontAwesomeIcon></p>
+                        <p className="m-0 fs-2 next-icon-item"><FontAwesomeIcon icon={faHandPointLeft}></FontAwesomeIcon></p>
                     </div>
                 </div>
                 <div className="col-10 h-100">
@@ -72,7 +72,7 @@ const FoodDisplayTwo = (props) => {
                 </div>
                 <div className="col-1 ">
                     <div className="next-icon  h-100">
-                        <p className="m-0 fs-2"><FontAwesomeIcon icon={faHandPointRight}></FontAwesomeIcon></p>
+                        <p className="m-0 fs-2 next-icon-item"><FontAwesomeIcon icon={faHandPointRight}></FontAwesomeIcon></p>
                     </div>
                 </div>
             </div>
