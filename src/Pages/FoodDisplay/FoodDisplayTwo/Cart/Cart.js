2732110379f379ef138510c80654e7ed21aef856
shafik720 <shafikrasel5@gmail.com>
2022-12-02 21:10:24 +0600

    working on single food display route

--- src/Pages/FoodDisplay/FoodDisplayTwo/Cart/Cart.js
+++ src/Pages/FoodDisplay/FoodDisplayTwo/Cart/Cart.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import useFood from '../../../../utilities/hooks/useFood';
 import { getStoredItem } from '../../../../utilities/LocalStorage/storage';
 
 const Cart = () => {
     const[cart, setCart] = useState([]);
+    const[food, setFood] = useFood();
     useEffect(()=>{
+        let newCart = [];
         let cartValue = getStoredItem();
-        console.log(cartValue);
-    },[cart])
+        for(let id in cartValue){
+            let addedProduct = food.find(index=>index.id == id);            
+            if(addedProduct){
+                addedProduct.quantity = cartValue[id];
+                newCart.push(addedProduct);
+            }
+        }
+        setCart(newCart);
+    },[food])
     return (
         <div>
-            <h2>Total Food Added : </h2>
+            <h2>Total Food Added : {cart.length}</h2>
         </div>
     );
 };
