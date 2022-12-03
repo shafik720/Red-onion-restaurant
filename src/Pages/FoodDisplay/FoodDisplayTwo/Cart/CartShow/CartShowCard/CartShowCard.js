6393c007ce7d09e7ae02f31a6bcc5fc8d4a71c86
shafik720 <shafikrasel5@gmail.com>
2022-12-03 14:45:23 +0600

    working on cart

--- src/Pages/FoodDisplay/FoodDisplayTwo/Cart/CartShow/CartShowCard/CartShowCard.js
+++ src/Pages/FoodDisplay/FoodDisplayTwo/Cart/CartShow/CartShowCard/CartShowCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './CartShowCard.css'
+import './CartShowCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 const CartShowCard = (props) => {
     const { foodName, shortDesc, price, catagory, img, id, quantity } = props.index;
@@ -10,13 +12,18 @@ const CartShowCard = (props) => {
                     <img src={img} alt="" />
                 </div>
                 <div className="foodcard-left-right">
-                        <p>Food Name : <span className='black-text'>{foodName}</span>  </p>
-                        <p>Quantity : <span className='blue-text'>{quantity}</span>  </p>
-                        <p>Price Per Unit : <span className='blue-text'> $ {price}</span> </p>
-                    </div>
+                    <p>Food Name : <span className='black-text'>{foodName}</span>  </p>
+                    <p>Quantity : <span className='blue-text'>{quantity}</span>  </p>
+                    <p>Price Per Unit : <span className='blue-text'> $ {price}</span> </p>
+                </div>
             </div>
             <div className="foodcard-right">
                 <p>Delete</p>
+                <div className="cart-button">
+                        <p className='working-button fw-bold fw-bolder'><FontAwesomeIcon className='working-button ' icon={faPlus}></FontAwesomeIcon></p>
+                        <p>0</p>
+                        <p className='working-button  fw-bold fw-bolder'><FontAwesomeIcon className='working-button ' icon={faMinus}></FontAwesomeIcon></p>
+                    </div>
             </div>
         </div>
     );
