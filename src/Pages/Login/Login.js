7a2dc144c3289a3211d98fc3e9ef39f388020943
shafik720 <shafikrasel5@gmail.com>
2022-12-03 19:05:45 +0600

    working on sign in  with email & password

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './Login.css';
 import signInLogo from '../../utilities/images/logo2.png';
+import auth from '../../firebase.init';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const Login = () => {
     let navigate = useNavigate();
     let location = useLocation();
     let from = location.state?.from?.pathname || "/";
+
+    // react fire base hook 
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+    // function for getting value from email input field
+    const [email, setEmail] = useState('');
+    function handleEmail(event) {
+        setEmail(event.target.value);
+    }
+
+    // function for getting value from password input field
+    const [password, setPassword] = useState('');
+    function handlePassword(event) {
+        setPassword(event.target.value);
+    }
+    function handleSubmit(e) {
+        e.preventDefault();              
+    }
+    if(user){
+        navigate('/');
+    }
     return (
         <div className='login-div'>
             <div className="login-header">
                 <img src={signInLogo} alt="" />
             </div>
             <div className="login-inputs">
-                <form action="">
-                    <input type="email" name="" id="" placeholder='Email'/>
-                    <input type="password" name="" id="" placeholder='Password' />
+                <form action="" onSubmit={handleSubmit}>
+                    <input onBlur={handleEmail} type="email" name="" id="" placeholder='Email'/>
+                    <input onBlur={handlePassword} type="password" name="" id="" placeholder='Password' />
                     <button type='submit'>Log In</button>
                     <p className='text-center'>New User ? <Link to='/signup'><span className='blue-text'>Register Now</span></Link> </p>
                 </form>
