d5883a8ce7c71b5028698753079107488113649b
shafik720 <shafikrasel5@gmail.com>
2022-12-03 18:19:32 +0600

    working on sign up with email & password

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -3,7 +3,7 @@ import './Signup.css';
 import signInLogo from '../../utilities/images/logo2.png';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
-import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
 
 const Signup = () => {
 
@@ -13,30 +13,36 @@ const Signup = () => {
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth);
 
-      // function for getting value from email input field
-      const[email, setEmail] = useState('');
-      function handleEmail(event){        
+    // function for getting value from email input field
+    const [email, setEmail] = useState('');
+    function handleEmail(event) {
         setEmail(event.target.value);
-      }
+    }
 
-      // function for getting value from password input field
-      const[password, setPassword] = useState('');
-      function handlePassword(event){
+    // function for getting value from password input field
+    const [password, setPassword] = useState('');
+    function handlePassword(event) {
         setPassword(event.target.value);
-      }
+    }
 
-      // function for getting value from re-password input field
-      const[rePassword, setRePassword] = useState('');
-      function handleRePassword(event){
+    // function for getting value from re-password input field
+    const [rePassword, setRePassword] = useState('');
+    function handleRePassword(event) {
         setRePassword(event.target.value);
-      }
+    }
 
-      // function for submitt button and sign up method
-      function handleSubmit(e){
+    // function for submitt button and sign up method
+    let [customError, setCustomError] = useState('');
+    function handleSubmit(e) {
         e.preventDefault();
-      }
+        if (password.length < 6) {
+            setCustomError('Password must be more than 6 Character');
+            return;
+        }
+        setCustomError('');
+    }
 
     return (
         <div className='login-div'>
@@ -46,9 +52,12 @@ const Signup = () => {
             <div className="login-inputs">
                 <form action="" onSubmit={handleSubmit}>
                     <input type="text" placeholder='Name' />
-                    <input onBlur={handleEmail} type="email" name="" id="" placeholder='Email'/>
+                    <input onBlur={handleEmail} type="email" name="" id="" placeholder='Email' />
                     <input onBlur={handlePassword} type="password" name="" id="" placeholder='Password' />
                     <input type="password" name="" id="" placeholder='Confirm Password' />
+                    <div className="error-display">
+                        <p>{customError ? customError : ''}</p>
+                    </div>
                     <button type='submit'>Register</button>
                     <p className='text-center'>Already Have an Account ? <Link to='/login'><span className='blue-text'>Log in Here</span></Link> </p>
                 </form>
