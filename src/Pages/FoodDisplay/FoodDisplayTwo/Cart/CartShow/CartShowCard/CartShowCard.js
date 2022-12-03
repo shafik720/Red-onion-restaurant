cf16716c92cba020d6b87ba2f3286d98f6948ec0
shafik720 <shafikrasel5@gmail.com>
2022-12-03 12:06:21 +0600

    working on cart

--- src/Pages/FoodDisplay/FoodDisplayTwo/Cart/CartShow/CartShowCard/CartShowCard.js
+++ src/Pages/FoodDisplay/FoodDisplayTwo/Cart/CartShow/CartShowCard/CartShowCard.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './CartShowCard.css'
 
 const CartShowCard = (props) => {
+    const { foodName, shortDesc, price, catagory, img, id } = props.index;
     return (
-        <div>
-            <h2>Food Name : </h2>
+        <div className='foodCard'>
+            <div className="foodcard-left">
+                <div className="foodcard-left-left">
+                    <img src={img} alt="" />
+                </div>
+            </div>
+            <div className="foodcard-right"></div>
+            <h2>Food Name : {foodName} </h2>
         </div>
     );
 };
